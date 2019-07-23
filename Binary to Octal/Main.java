d87d3ee4949535cdd600059fd275d0e02e05a5d6
samivicky <52149699+samivicky@users.noreply.github.com>
2019-07-23 17:50:34 +0530

    Test code pushed to repository.

---
+++ Binary to Octal/Main.java
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int power_fn(int base,int exp)
+{
+  int i,sum=1;
+  for(i=0;i<exp;i++)
+  {
+    sum=sum*base;
+  }
+  return sum;
+}
+
+int main()
+{
+  int bin,sum=0,rem,exp=0;
+  scanf("%d",&bin);
+  while(bin!=0)
+  {
+    rem=bin%10;
+    sum=sum+rem*power_fn(2,exp);
+    exp++;
+    bin=bin/10;
+  }
+  int arr[100],i=0,j;
+    while(sum!=0)
+    {
+      arr[i++]=sum%8;
+      sum=sum/8;
+    }
+  for(j=i-1;j>=0;j--)
+    printf("%d",arr[j]);
+}
